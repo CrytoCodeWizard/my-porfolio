4751be74345b55e36302afc447580a33b668e262
StarryNightSky <starrynightskyhoney@gmail.com>
2024-04-24 13:47:27 +0200

    update: my phone number

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/abu-said-bd/" className="text-[#16f2b3]">Juan Santos</Link>
+            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/" className="text-[#16f2b3]">Juan Santos</Link>
           </p>
           <div className="flex items-center gap-5">
             <Link
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -4,7 +4,7 @@ export const personalData = {
   designation: "Software Engineer",
   description: "I'm Juan Santos, a dynamic and dedicated software engineer deeply engaged in the ever-evolving world of web development.My journey in programming is driven by an insatiable curiosity and a self-motivated approach to learning.With a particular affinity for JavaScript, I excel in leveraging its capabilities to craft seamless and innovative web applications.My passion lies in both the depth and breadth of web technologies, striving to contribute to a more connected and accessible digital world.Beyond just coding, I thrive on solving complex challenges and transforming ideas into reality. Currently,I am exploring opportunities where my skills and passions can be aligned and where I can make a significant impact in advancing web technologies and user experiences.",
   email: 'cryptowizard0402@gmail.com',
-  phone: '+1 210 591 7670',
+  phone: '+63 45 320 9252',
   address: 'MAPULANG LUPA CITY, BULACAN, PHILIPPINES - VX7J V2H',
   github: 'https://github.com/CrytoCodeWizard',
   facebook: 'https://www.facebook.com/',
